f25871b2f74ff94d5573e0f9955715695ba1e4f2
TheWayToJunior <developermicrosoft1932@gmail.com>
2021-11-25 16:55:58 +0300

    Typo fix

--- src/Ember.Application/Specification/RolesSpecificationsFactory.cs
+++ src/Ember.Application/Specification/RolesSpecificationsFactory.cs
@@ -7,26 +7,26 @@ namespace Ember.Application.Specification
 {
     public class RolesSpecificationsFactory
     {
-        protected virtual ISpecification CreateAdminRoleSpecification(IEnumerable<string> usetRoles, IEnumerable<string> changRoles)
+        protected virtual ISpecification CreateAdminRoleSpecification(IEnumerable<string> userRoles, IEnumerable<string> changRoles)
         {
             var condition = new DelegateCondition<IEnumerable<string>>(roles => roles.Contains(Roles.Admin));
 
             return new OrSpecification(
-                new NoChangeRoleSpecification(usetRoles, condition),
+                new NoChangeRoleSpecification(userRoles, condition),
                 new NoChangeRoleSpecification(changRoles, condition));
         }
 
-        protected virtual ISpecification CreateUserRoleSpecification(IEnumerable<string> roles)
+        protected virtual ISpecification CreateUserRoleSpecification(IEnumerable<string> userRoles, IEnumerable<string> changRoles)
         {
-            return new NoChangeRoleSpecification(roles,
+            return new NoChangeRoleSpecification(changRoles,
                 new DelegateCondition<IEnumerable<string>>(@roles => !@roles.Contains(Roles.User)));
         }
 
-        public SpecificationsCollection Create(IEnumerable<string> usetRoles, IEnumerable<string> changRoles)
+        public SpecificationsCollection Create(IEnumerable<string> userRoles, IEnumerable<string> changRoles)
         {
             return new SpecificationsCollection(
-                CreateAdminRoleSpecification(usetRoles, changRoles),
-                CreateUserRoleSpecification(changRoles));
+                CreateAdminRoleSpecification(userRoles, changRoles),
+                CreateUserRoleSpecification(null, changRoles));
         }
     }
 }
